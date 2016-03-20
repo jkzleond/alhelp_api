0c3910109732b9e58d90cbc39f9a463da2a5173f
jkzleond <jkzleond@163.com>
2016-03-20 23:00:30 +0800

    haha

--- Api/Controller/UploadController.class.php
+++ Api/Controller/UploadController.class.php
@@ -82,7 +82,7 @@ class UploadController extends ApiBaseController {
 					'data' => $tmp
 				))), $redirect_url);
 				echo $redirect_url;
-				$this->redirect($redirect_url);
+				//$this->redirect($redirect_url);
 				return;
 			}
 			$this->success ( $tmp );
